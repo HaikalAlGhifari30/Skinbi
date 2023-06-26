b9381eb57f58610c5b501246821d394b35c2481f
HaikalAlGhifari30 <alghifaribahren03@gmail.com>
2023-06-26 12:50:36 +0700

    git commit -m

--- app/src/main/java/com/example/skinbiapp/Signin_Activity.kt
+++ app/src/main/java/com/example/skinbiapp/Signin_Activity.kt
@@ -3,15 +3,34 @@ package com.example.skinbiapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import android.widget.Toast
+import com.example.skinbiapp.databinding.ActivitySigninBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Signin_Activity : AppCompatActivity() {
+
+    lateinit var binding : ActivitySigninBinding
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+
+
+
+        binding.btnregister.setOnClickListener {
+            val intent = Intent(this, signup_activity::class.java )
+            startActivity(intent)
+        }
 
         val tologin = findViewById<Button>(R.id.btnregiscamera)
         val toregister = findViewById<Button>(R.id.btnregister)
+        val tobacklogin = findViewById<Button>(R.id.btnbcklogin)
 
         tologin.setOnClickListener {
             Intent(this, Home_Activity::class.java).also {
@@ -24,5 +43,61 @@ class Signin_Activity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+
+        tobacklogin.setOnClickListener {
+            Intent(this, Signin_Activity::class.java).also {
+               startActivity(it)
+            }
+        }
+
+        binding.btnregiscamera.setOnClickListener {
+            val email = binding.textemailsign.text.toString()
+            val password = binding.textpasssign.text.toString()
+
+            //validasi email tidak sesuai
+            if (email.isEmpty()) {
+                binding.textemailsign.error = "Email Harus Diisi"
+                binding.textemailsign.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi email tidal valid
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.textemailsign.error = "Email Tidak Valid"
+                binding.textemailsign.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi password tidak sesuai
+            if (password.isEmpty()) {
+                binding.textpasssign.error = "Password Harus Diisi"
+                binding.textpasssign.requestFocus()
+                return@setOnClickListener
+            }
+
+
+
+            LoginFirebase(email,password)
+        }
+    }
+    private fun LoginFirebase(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+                    Toast.makeText(this, "Selamat Datang $email ", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,Signin_Activity::class.java)
+                    startActivity(intent)
+
+                }else{
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
     }
 }
+
+
+
+
+
--- app/src/main/java/com/example/skinbiapp/signup_activity.kt
+++ app/src/main/java/com/example/skinbiapp/signup_activity.kt
@@ -3,19 +3,101 @@ package com.example.skinbiapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import android.util.Patterns
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.example.skinbiapp.databinding.ActivitySignupBinding
+import com.google.firebase.auth.FirebaseAuth
+import java.net.PasswordAuthentication
+import java.util.regex.Pattern
 
 class signup_activity : AppCompatActivity() {
+    lateinit var editfullName: EditText
+    lateinit var editEmail: EditText
+    lateinit var password : EditText
+    lateinit var binding: ActivitySignupBinding
+    lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySignupBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        setContentView(binding.root)
 
-        val toregis = findViewById<Button>(R.id.btnregiscamera)
+
+        auth = FirebaseAuth.getInstance()
+
+        binding.btnbcklogin.setOnClickListener {
+            val intent = Intent(this, Signin_Activity::class.java)
+            startActivity(intent)
+
+        }
+
+        binding. btnregiscamera.setOnClickListener {
+            val email = binding.textView4.text.toString()
+            val password = binding.textView25.text.toString()
+
+            //validasi email tidak sesuai
+            if (email.isEmpty()) {
+                binding.textView4.error = "Email Harus Diisi"
+                binding.textView4.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi email tidal valid
+            if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                binding.textView4.error = "Email Tidak Valid"
+                binding.textView4.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi password tidak sesuai
+            if (password.isEmpty()) {
+                binding.textView25.error = "Password Harus Diisi"
+                binding.textView25.requestFocus()
+                return@setOnClickListener
+            }
+
+            //validasi panjang passsword
+            if (password.length < 6){
+                binding.textView25.error = "Password Minimal 6 Karakter"
+                binding.textView25.requestFocus()
+                return@setOnClickListener
+            }
+            
+            RegisterFirebase(email,password)
+
+        }
+
+
+
+            val toregis = findViewById<Button>(R.id.btnregiscamera)
+
+        editfullName = findViewById(R.id.textView3)
+        editEmail = findViewById(R.id.textView4)
+        password = findViewById(R.id.textView25)
 
         toregis.setOnClickListener {
             Intent(this, Camera_Register_Activity::class.java).also {
                 startActivity(it)
             }
         }
+    }
+
+    private fun RegisterFirebase(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this){
+                if (it.isSuccessful){
+
+                    Toast.makeText(this, "Register Berhasi;", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(this,Signin_Activity::class.java)
+                    startActivity(intent)
+
+                }else{
+                    Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+
     }
 }
